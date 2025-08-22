5b8e776b8f1d99d7fac4ffe9482b409c2adfdc3f
raudikon <raudikon@gmail.com>
2025-08-22 18:01:03 -0400

    put minified frontend in root and updated baseurl in frontend

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+//morgan
 var morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(express.static('dist'))
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
